6b10d9b19fce87a5a8ac036bc23df3b45224f00e
Francesco Sullo <sullof@gmail.com>
2023-05-17 16:07:24 -0700

    First version

---
+++ docker.sh
@@ -0,0 +1,10 @@
+docker stop baac-us
+docker rm baac-us
+
+docker run --name baac-us \
+  -p 8040 \
+  --restart unless-stopped \
+  -e VIRTUAL_HOST=baac.us,www.baac.us \
+  -e LETSENCRYPT_HOST=baac.us,www.baac.us \
+  -e LETSENCRYPT_EMAIL=francesco@sullo.co \
+  -v `pwd`/baac.us/html:/usr/share/nginx/html:ro -d nginx
